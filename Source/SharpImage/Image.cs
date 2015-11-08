c5ea876cc8a50dd7a0a3c8c222bc3d009efc36ae
smack0007 <zachary.snow@gmail.com>
2015-11-08 21:18:32 +0100

    Initial checkin.

---
+++ Source/SharpImage/Image.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpImage
+{
+    public class Image
+    {
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public PixelFormat PixelFormat { get; private set; }
+
+        public byte[] Pixels { get; private set; }
+
+        public Image(int width, int height, PixelFormat pixelFormat, byte[] pixels)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.PixelFormat = pixelFormat;
+            this.Pixels = pixels;
+        }
+
+        public static ImageFormat GetImageFormatFromFileName(string fileName)
+        {
+            string fileExtension = Path.GetExtension(fileName).Substring(1).ToLower();
+
+            switch (fileExtension)
+            {
+                case "tga":
+                    return ImageFormat.Tga;
+            }
+
+            throw new InvalidOperationException($"ImageFormat cannot be determined for file {fileName}. Use overload which specifies the ImageFormat.");
+        }
+
+        public static Image FromFile(string fileName)
+        {
+            using (FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                return FromStream(stream, GetImageFormatFromFileName(fileName));
+            }
+        }
+
+        public static Image FromStream(Stream stream, ImageFormat format)
+        {
+            switch (format)
+            {
+                case ImageFormat.Tga:
+                    return Tga.FromStream(stream);
+            }
+
+            throw new NotImplementedException("ImageFormat." + format.ToString() + " not implemented.");
+        }
+    }
+}
---
+++ Source/SharpImage/Tga.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace SharpImage
+{
+    public static class Tga
+    {
+        public static Image FromStream(Stream stream)
+        {
+            BinaryReader br = new BinaryReader(stream);
+            
+            byte[] header = br.ReadBytes(18);
+
+            bool isCompressed = header[2] == 10;
+            short width = (short)((header[13] << 8) | header[12]);
+            short height = (short)((header[15] << 8) | header[14]);
+            byte bpp = header[16];
+
+            int dataLength = width * height * bpp;
+            byte[] pixels = br.ReadBytes(dataLength);
+
+            for (int i = 0; i < pixels.Length; i += bpp)
+            {
+                byte temp = pixels[i];
+                pixels[i] = pixels[i + 2];
+                pixels[i + 2] = temp;
+            }
+
+            return new Image(width, height, PixelFormat.R8G8B8A8, pixels);
+        }
+
+    }
+}
