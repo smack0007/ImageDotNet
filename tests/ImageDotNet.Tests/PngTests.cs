9417abaf091672d60cbaec34a97ee3f9b132d6f1
smack0007 <zachary.snow@gmail.com>
2018-10-23 21:16:50 +0200

    Code generator now produces unit tests for file formats.

--- tests/ImageDotNet.Tests/PngTests.cs
+++ tests/ImageDotNet.Tests/PngTests.cs
@@ -1,10 +1,9 @@
 ﻿using System.IO;
-using Xunit;
 using Xunit.Abstractions;
 
 namespace ImageDotNet.Tests
 {
-    public class PngTests
+    public partial class PngTests
     {
         private readonly ITestOutputHelper _output;
 
@@ -13,54 +12,19 @@ namespace ImageDotNet.Tests
             _output = output;
         }
 
-        private static readonly Rgba32[] ImageData1x1 = new Rgba32[]
+        private static void AssertSaveAndLoad<T>(Image<T> expected)
+            where T : unmanaged, IPixel
         {
-            new Rgba32(1, 2, 3, 4),
-        };
-
-        private static readonly Rgba32[] ImageData2x2 = new Rgba32[]
-        {
-            new Rgba32(1, 2, 3, 4), new Rgba32(5, 6, 7, 8),
-            new Rgba32(9, 10, 11, 12), new Rgba32(13, 14, 15, 16),
-        };
-
-        private static readonly Rgba32[] ImageData3x3 = new Rgba32[]
-        {
-            new Rgba32(1, 2, 3, 4), new Rgba32(5, 6, 7, 8), new Rgba32(9, 10, 11, 12),
-            new Rgba32(13, 14, 15, 16), new Rgba32(17, 18, 19, 20), new Rgba32(21, 22, 23, 24),
-            new Rgba32(25, 26, 27, 28), new Rgba32(29, 30, 31, 32), new Rgba32(33, 34, 35, 36),
-        };
-
-        private static void AssertSaveAndLoad(Image<Rgba32> expected)
-        {
-            Image<Rgba32> actual = null;
+            Image<T> actual = null;
 
             using (var memory = new MemoryStream())
             {
                 expected.SavePng(memory);
                 memory.Position = 0;
-                actual = (Image<Rgba32>)Image.LoadPng(memory);
+                actual = Image.LoadPng(memory).To<T>();
             }
 
             AssertEx.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AfterSaveAndLoadPixelsAreTheSame1x1Image()
-        {
-            AssertSaveAndLoad(new Image<Rgba32>(1, 1, ImageData1x1));
-        }
-
-        [Fact]
-        public void AfterSaveAndLoadPixelsAreTheSame2x2Image()
-        {
-            AssertSaveAndLoad(new Image<Rgba32>(2, 2, ImageData2x2));
-        }
-
-        [Fact]
-        public void AfterSaveAndLoadPixelsAreTheSame3x3Image()
-        {
-            AssertSaveAndLoad(new Image<Rgba32>(3, 3, ImageData3x3));
         }
     }
 }
--- tests/ImageDotNet.Tests/TgaTests.cs
+++ tests/ImageDotNet.Tests/TgaTests.cs
@@ -4,7 +4,7 @@ using Xunit.Abstractions;
 
 namespace ImageDotNet.Tests
 {
-    public class TgaTests
+    public partial class TgaTests
     {
         private readonly ITestOutputHelper _output;
 
@@ -13,54 +13,19 @@ namespace ImageDotNet.Tests
             _output = output;
         }
 
-        private static readonly Bgra32[] ImageData1x1 = new Bgra32[]
+        private static void AssertSaveAndLoad<T>(Image<T> expected)
+            where T : unmanaged, IPixel
         {
-            new Bgra32(1, 2, 3, 4),
-        };
-
-        private static readonly Bgra32[] ImageData2x2 = new Bgra32[]
-        {
-            new Bgra32(1, 2, 3, 4), new Bgra32(5, 6, 7, 8),
-            new Bgra32(9, 10, 11, 12), new Bgra32(13, 14, 15, 16),
-        };
-
-        private static readonly Bgra32[] ImageData3x3 = new Bgra32[]
-        {
-            new Bgra32(1, 2, 3, 4), new Bgra32(5, 6, 7, 8), new Bgra32(9, 10, 11, 12),
-            new Bgra32(13, 14, 15, 16), new Bgra32(17, 18, 19, 20), new Bgra32(21, 22, 23, 24),
-            new Bgra32(25, 26, 27, 28), new Bgra32(29, 30, 31, 32), new Bgra32(33, 34, 35, 36),
-        };
-
-        private static void AssertSaveAndLoad(Image<Bgra32> expected)
-        {
-            Image<Bgra32> actual = null;
+            Image<T> actual = null;
 
             using (var memory = new MemoryStream())
             {
                 expected.SaveTga(memory);
                 memory.Position = 0;
-                actual = (Image<Bgra32>)Image.LoadTga(memory);
+                actual = Image.LoadTga(memory).To<T>();
             }
 
             AssertEx.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AfterSaveAndLoadPixelsAreTheSame1x1Image()
-        {
-            AssertSaveAndLoad(new Image<Bgra32>(1, 1, ImageData1x1));
-        }
-
-        [Fact]
-        public void AfterSaveAndLoadPixelsAreTheSame2x2Image()
-        {
-            AssertSaveAndLoad(new Image<Bgra32>(2, 2, ImageData2x2));
-        }
-
-        [Fact]
-        public void AfterSaveAndLoadPixelsAreTheSame3x3Image()
-        {
-            AssertSaveAndLoad(new Image<Bgra32>(3, 3, ImageData3x3));
         }
     }
 }
